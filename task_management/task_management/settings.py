219313efd7bfbd062eb1111ec68b37e1d1417845
rex <rex.honey@fintoo.in>
2023-08-25 00:44:17 +0530

    static root

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_DIR= os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS=[STATIC_DIR,]
+# STATICFILES_DIRS=[STATIC_DIR,]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
