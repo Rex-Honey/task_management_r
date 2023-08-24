5babfd65a14443e5e75cfcbc7ad31cb406e0f923
rex <rex.honey@fintoo.in>
2023-08-24 23:43:35 +0530

    allowed host

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 AUTH_USER_MODEL='authentication.CustomUser'
 # Application definition
-
+ALLOWED_HOSTS = ['rexhoney.pythonanywhere.com']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
