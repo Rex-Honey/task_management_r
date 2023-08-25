fc6481522d613d18bd7e19256e4ef29646cf219d
rex <rex.honey@fintoo.in>
2023-08-25 08:03:25 +0530

    restframework

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -104,8 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-    # 'rest_framework.authentication.SessionAuthentication',
-    'rest_framework.authentication.BasicAuthentication',
+    'rest_framework.authentication.SessionAuthentication',
+    # 'rest_framework.authentication.BasicAuthentication',
 ]
 }
 
